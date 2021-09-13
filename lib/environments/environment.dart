2c3a531a357c83c40e651cfeef231d55ab10617c
Ludwig Linder <ludwigclinder@gmail.com>
2021-09-13 17:22:28 +0200

    clean contacts

--- lib/environments/environment.dart
+++ lib/environments/environment.dart
@@ -1,4 +1,4 @@
 class Environment {
-  static const String API_URL = "https://stage.fsektionen.se";
+  static const String API_URL = "https://fsektionen.se";
   static const String CABLE_URL = "wss://stage.fsektionen.se/cable";
 }
