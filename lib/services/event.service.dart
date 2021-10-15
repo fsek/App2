5c0c864c359e061b4bfdcea6b6fac8cb5fe1b2be
snelhest1337 <60144469+snelhest1337@users.noreply.github.com>
2021-10-15 13:49:25 +0200

    Fix time stuff (#98)
    * Fixed food pref side effects on event page
    
    Fixed the food pref issue. Also added extra space
    at bottom of calendar view to prevent overlap with
    nollning button.
    
    * Fixes various time bugs
    
    Fixed time zone issue between 00:00 and 02:00,
    fixed hard coded event interval in service

--- lib/services/event.service.dart
+++ lib/services/event.service.dart
@@ -1,18 +1,35 @@
 import 'package:fsek_mobile/models/home/event.dart';
 import 'package:fsek_mobile/models/home/calendarevent.dart';
+import 'package:intl/intl.dart';
 
 import 'abstract.service.dart';
 
 class EventService extends AbstractService {
   Future<Map<DateTime, List<CalendarEvent>>> getEvents() async {
     Map<DateTime, List<CalendarEvent>> result = {};
-    Map json = await AbstractService.get("/events?start=20200101&end=20220813");
+
+    Map json = await AbstractService.get("/events?start=" +
+        DateFormat("y", "sv_SE")
+            .format(DateTime.now().subtract(Duration(days: 365))) +
+        DateFormat("M", "sv_SE")
+            .format(DateTime.now().subtract(Duration(days: 365))) +
+        DateFormat("dd", "sv_SE")
+            .format(DateTime.now().subtract(Duration(days: 365))) +
+        "&end=" +
+        DateFormat("y", "sv_SE")
+            .format(DateTime.now().add(Duration(days: 365))) +
+        DateFormat("M", "sv_SE")
+            .format(DateTime.now().add(Duration(days: 365))) +
+        DateFormat("dd", "sv_SE")
+            .format(DateTime.now().add(Duration(days: 365))));
     List<CalendarEvent> eventlist = (json['events'] as List)
         .map((data) => CalendarEvent.fromJson(data))
         .toList();
     eventlist.forEach((event) {
-      DateTime? eventDay = DateTime.utc((event.start?.year ?? 2021),
-          (event.start?.month ?? 08), (event.start?.day ?? 08));
+      DateTime? eventDay = DateTime.utc(
+          (event.start?.toLocal().year ?? 2010),
+          (event.start?.toLocal().month ?? 01),
+          (event.start?.toLocal().day ?? 01));
       if (result.containsKey(eventDay)) {
         result[eventDay]?.add(event);
       } else {
