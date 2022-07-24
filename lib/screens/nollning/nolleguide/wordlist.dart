779b2411c8eb9d9a8ff8c8fceb706762cd58b0a4
snelhest1337 <emil.manelius@gmail.com>
2022-07-24 20:45:50 +0200

    Added wordlist to nolleguide
    * added wordlist to nolleguiden
    
    * added assets with text content for nolleguide

--- lib/screens/nollning/nolleguide/wordlist.dart
+++ lib/screens/nollning/nolleguide/wordlist.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:fsek_mobile/models/nollning/nolleguide/word.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
 class WordListPage extends StatefulWidget {
   @override
@@ -6,11 +10,70 @@ class WordListPage extends StatefulWidget {
 }
 
 class _WordListState extends State<WordListPage> {
+  List<Word> wordlist = [];
+
+  _WordListState() {
+    _loadWordList().then(
+      (value) => setState(
+        () {
+          this.wordlist = value;
+        },
+      ),
+    );
+  }
+
+  Future<List<Word>> _loadWordList() async {
+    final String raw =
+        await rootBundle.loadString('assets/data/guide/wordlist.json');
+    Map<String, dynamic> jsonRaw = await json.decode(raw);
+    List<Word> out =
+        (jsonRaw['words'] as List).map((data) => Word.fromJson(data)).toList();
+    return out;
+  }
+
+  Widget _createWordCard(Word w) {
+    return Column(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          child: InkWell(
+            onTap: () {},
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  w.key!,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                Text(w.value!, style: TextStyle(fontSize: 18))
+              ],
+            ),
+          ),
+        ),
+        SizedBox(height: 10),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Ordlista"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(20),
+        child: ListView(
+          children: [
+            Column(
+              children: [
+                ...wordlist.map(
+                  (Word w) => _createWordCard(w),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
