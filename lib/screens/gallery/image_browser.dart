681851462fd696fd8d68e16b6104fa8bc03e4f51
Dick Dastardly <si1854lo-s@student.lu.se>
2022-09-28 16:29:33 +0200

    Image saving fixed.

--- lib/screens/gallery/image_browser.dart
+++ lib/screens/gallery/image_browser.dart
@@ -3,6 +3,8 @@ import 'package:fsek_mobile/environments/environment.dart';
 import 'package:fsek_mobile/models/gallery/album.dart';
 import 'package:fsek_mobile/widgets/loading_widget.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_save/image_save.dart';
 
 class ImageBrowserPage extends StatelessWidget {
   const ImageBrowserPage({Key? key, required this.album, required this.initial})
@@ -44,7 +46,15 @@ class ImageBrowserPage extends StatelessWidget {
           Text(
             "${t.galleryTitle} ${index + 1} ${t.galleryOf} ${album.images!.length}",
             style: Theme.of(context).textTheme.headline6,
-          )
+          ),Padding(padding: EdgeInsets.fromLTRB(0, 15, 0, 0)),
+                  FloatingActionButton(child: Icon(Icons.download), onPressed: () async {
+                    http.Response image = await http.get(
+                        Uri.parse("${Environment.API_URL}${album.images![index].file!.large!["url"]!}")
+                    );
+                    ImageSave.saveImage(image.bodyBytes, "${album.images![index].filename!}");
+                    //This could probably be done better.
+
+                  }),
         ])),
       ),
     );
