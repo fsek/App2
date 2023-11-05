62dea81326c632f354860bacd8628a9b730debf3
william-lundgren <superbanan@live.se>
2023-11-05 23:56:44 +0100

    Added new text for about guild meeting and updated candidacy poster

--- lib/widgets/easterEgg/easteregg_code_dialog.dart
+++ lib/widgets/easterEgg/easteregg_code_dialog.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 Future<String?> easterEggCodeDialog(BuildContext context) {
   TextEditingController _textFieldController = TextEditingController();
-  var t = AppLocalizations.of(context)!; //what are u translatng????????
   return showDialog(
     context: context,
     builder: (context) {
