858b99053a51b791c92d211f09b2c34f4e879930
william-lundgren <superbanan@live.se>
2023-12-03 15:00:21 +0100

    Added functionality for background changes from website

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:fsek_mobile/screens/guild_meeting/candidacy_poster.dart';
 import 'package:fsek_mobile/screens/guild_meeting/other_documents.dart';
 import 'package:fsek_mobile/screens/guild_meeting/about_guild_meeting.dart';
 import 'package:fsek_mobile/screens/guild_meeting/propositions.dart';
 import 'package:fsek_mobile/screens/guild_meeting/motions.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:fsek_mobile/models/documents/election_document.dart';
+import 'package:fsek_mobile/screens/guild_meeting/proposition_card.dart';
+import 'package:fsek_mobile/services/service_locator.dart';
+import 'package:fsek_mobile/services/document.service.dart';
 
 class HomePage extends StatefulWidget {
   static const routeName = '/homepage';
@@ -14,7 +24,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<ElectionDocument>? backgroundDocuments;
+  String? backgroundUrl;
+
   void initState() {
+    locator<DocumentService>().getOthers("Bakgrund").then((value) => setState(() {
+          // Value is null of getothers parameter doesnt exist, empty list if it exists but no documents in it.
+          if (!listEquals(value, []) && value != null) {
+            this.backgroundDocuments = value;
+            this.backgroundUrl = value.last.url;
+          }
+        }));
     super.initState();
   }
 
@@ -22,23 +42,31 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     var t = AppLocalizations.of(context)!;
     double edgePadding = MediaQuery.of(context).size.width / 25;
-    String backgroundPath = "assets/img/ht_23_background.png";
+    String defaultBackground = "assets/img/default_background.png";
+
     return Stack(children: [
-      Image.asset(
-        backgroundPath,
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        fit: BoxFit.cover,
-        alignment: Alignment.topCenter,
-      ),
+      // If we couldnt get a background image for whatever reason make it the default
+      backgroundUrl != null
+          ? CachedNetworkImage(
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter,
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              placeholder: (context, url) => const Center(child: CircularProgressIndicator(color: Color(0xFFFB8C00))),
+              imageUrl: backgroundUrl!,
+            )
+          : Image.asset(
+              defaultBackground,
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter,
+            ),
       Scaffold(
         backgroundColor: Colors.transparent,
         body: Padding(
           padding: EdgeInsets.fromLTRB(
-              edgePadding,
-              MediaQuery.of(context).size.height / 2.69420 /* lemao */,
-              edgePadding,
-              0),
+              edgePadding, MediaQuery.of(context).size.height / 2.69420 /* lemao */, edgePadding, 0),
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -46,6 +74,7 @@ class _HomePageState extends State<HomePage> {
                 Spacer(flex: 3),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  // TODO Change the text and function for generic version
                   children: [
                     button(t.guildMeetingButtonAbout, AboutGuildMeetingPage()),
                     button(t.guildMeetingButtonPoster, CandidacyPosterPage()),
@@ -81,16 +110,20 @@ class _HomePageState extends State<HomePage> {
   Widget button(String text, Widget destination) {
     return TextButton(
       onPressed: () {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => destination));
+        Navigator.push(context, MaterialPageRoute(builder: (context) => destination));
       },
-      child: Text(text),
+      child: Text(
+        text,
+        style: TextStyle(color: Colors.black),
+      ),
       style: TextButton.styleFrom(
           padding: EdgeInsets.zero,
-          backgroundColor: Color(0xFF667760).withOpacity(0.6),
+          backgroundColor: Colors.white.withOpacity(1.0),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(12),
           ),
+          shadowColor: Colors.black,
+          elevation: 10,
           minimumSize: Size(MediaQuery.of(context).size.width / 2.4, 80)),
     );
   }
