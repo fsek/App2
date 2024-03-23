1b550a5cdf2f20973eb29a9ef04763f5599a51c5
Moa Söderström <moissoderstrom@gmail.com>
2024-03-23 14:20:14 +0100

    two new files

---
+++ lib/services/book-exchange.service.dart
@@ -0,0 +1,21 @@
+import 'package:fsek_mobile/models/songbook/songbookEntry.dart';
+
+import 'package:fsek_mobile/services/abstract.service.dart';
+
+class SongbookService extends AbstractService {
+  Future<List<SongbookEntry>> getSongbook() async {
+    Map json = await AbstractService.get("/songs");
+    List<SongbookEntry> result = (json['songs'] as List)
+        .map((data) => SongbookEntry.fromJson(data))
+        .toList();
+    return result;
+  }
+
+  Future<List<SongbookEntry>> getChantbook() async {
+    Map json = await AbstractService.get("/songs/chants");
+    List<SongbookEntry> result = (json['songs'] as List)
+        .map((data) => SongbookEntry.fromJson(data))
+        .toList();
+    return result;
+  }
+}
---
+++ lib/services/book.service.dart
@@ -0,0 +1,11 @@
+import 'package:fsek_mobile/models/songbook/song.dart';
+
+import 'abstract.service.dart';
+
+class SongService extends AbstractService {
+  Future<Song> getSong(int id) async {
+    Map json = await AbstractService.get('/songs/$id');
+    Song result = Song.fromJson(json['song']);
+    return result;
+  }
+}
