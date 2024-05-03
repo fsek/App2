65a75fe2f184632c9f5cd0c1486fc6b9485e442b
Georg Elgeback <georgelgeback@gmail.com>
2024-05-03 18:01:05 +0200

    Emergency contact numbers made clickable

--- lib/screens/nollning/emergency_contacts.dart
+++ lib/screens/nollning/emergency_contacts.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:linkable/linkable.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 //TODO: fixa länkar i texten.
 
@@ -37,34 +38,43 @@ class _EmergencyContactsPageState extends State<EmergencyContactsPage> {
         children: <Widget>[
           _EmergencyText(
             headerText: t.emergencyTitleOne,
+            headerPhoneNumber: t.emergencyNumOne,
             bodyText: t.emergencyContentOne,
           ),
           _EmergencyText(
             headerText: t.emergencyTitleTwo,
+            headerPhoneNumber: t.emergencyNumTwo,
             bodyText: t.emergencyContentTwo,
           ),
           _EmergencyText(
             headerText: t.emergencyTitleThree,
+            headerPhoneNumber: t.emergencyNumThree,
             bodyText: t.emergencyContentThree,
           ),
           _EmergencyText(
             headerText: t.emergencyTitleFour,
+            headerPhoneNumber: t.emergencyNumFour,
             bodyText: t.emergencyContentFour,
           ),
           _EmergencyText(
             headerText: t.emergencyTitleFive,
+            headerPhoneNumber: t.emergencyNumFive,
             bodyText: t.emergencyContentFive,
           ),
           _EmergencyText(
             headerText: t.emergencyTitleSix,
+            headerPhoneNumber: t.emergencyNumSix,
             bodyText: t.emergencyContentSix,
           ),
           _EmergencyText(
             headerText: t.emergencyTitleSeven,
+            headerPhoneNumber: t.emergencyNumSeven,
             bodyText: t.emergencyContentSeven,
           ),
           _EmergencyText(
             headerText: t.emergencyTitleEight,
+            // Dummy number because this title is different
+            headerPhoneNumber: "",
             bodyText: t.emergencyContentEight,
           )
         ],
@@ -75,16 +85,20 @@ class _EmergencyContactsPageState extends State<EmergencyContactsPage> {
 
 class _EmergencyText extends StatelessWidget {
   final String headerText;
+  final String headerPhoneNumber;
   final String bodyText;
 
-  _EmergencyText({required this.headerText, required this.bodyText});
+  _EmergencyText(
+      {required this.headerText,
+      required this.headerPhoneNumber,
+      required this.bodyText});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
-          _HeaderText(headerText),
+          _HeaderText(headerText, headerPhoneNumber),
           _BodyText(bodyText),
         ],
       ),
@@ -93,24 +107,49 @@ class _EmergencyText extends StatelessWidget {
 }
 
 class _HeaderText extends StatelessWidget {
+  final String phoneNumber;
   final String text;
+  final TextStyle headerNumStyle = TextStyle(
+    fontSize: 25.0,
+    color: Colors.blue,
+  );
   final TextStyle headerStyle = TextStyle(
     fontSize: 25.0,
     color: Colors.black,
   );
   final Color? color = Colors.grey[200];
 
-  _HeaderText(this.text);
+  Future<void> _launchPhoneNumber(String phoneNumber) async {
+    // phoneNumber can contain "-" and " ", launchUri removes these
+    final Uri launchUri = Uri(
+      scheme: 'tel',
+      path: phoneNumber,
+    );
+    await launchUrl(launchUri);
+  }
+
+  _HeaderText(this.text, this.phoneNumber);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Container(
-          margin: const EdgeInsets.all(10),
-          alignment: Alignment.centerLeft,
-          child: Row(children: [
-            Flexible(child: Linkable(text: text, style: headerStyle))
-          ])),
+        margin: const EdgeInsets.all(10),
+        alignment: Alignment.centerLeft,
+        child: InkWell(
+          onTap: () {
+            _launchPhoneNumber(phoneNumber);
+          },
+          child: RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(text: phoneNumber, style: headerNumStyle),
+                TextSpan(text: text, style: headerStyle),
+              ],
+            ),
+          ),
+        ),
+      ),
       color: color,
     );
   }
