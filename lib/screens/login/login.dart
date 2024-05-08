5f353d0d7a677181176e62a6461d0070833aaa2d
william-lundgren <superbanan@live.se>
2024-05-08 22:18:34 +0200

    so much stuff trying to get 3.19 working

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:fsek_mobile/services/user.service.dart';
 import 'package:fsek_mobile/util/authentication/authentication_bloc.dart';
 import 'package:fsek_mobile/util/authentication/login_bloc.dart';
@@ -28,8 +28,7 @@ class _LoginPageState extends State<LoginPage> {
   void initState() {
     _userService = widget.userService;
     _authenticationBloc = BlocProvider.of<AuthenticationBloc>(context);
-    _loginBloc = LoginBloc(
-        authenticationBloc: _authenticationBloc!, userService: _userService!);
+    _loginBloc = LoginBloc(authenticationBloc: _authenticationBloc!, userService: _userService!);
 
     if (!kIsWeb) {
       PackageInfo.fromPlatform().then((PackageInfo packageInfo) {
@@ -43,17 +42,14 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    var _versionText = Text("F-appen Version: " + buildName,
-        style: Theme.of(context).textTheme.bodyLarge);
+    var _versionText = Text("F-appen Version: " + buildName, style: Theme.of(context).textTheme.bodyLarge);
     return Scaffold(
         backgroundColor: Colors.transparent,
         body: Stack(children: [
           Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [_versionText]),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [_versionText]),
               SizedBox(
                 height: 12,
               )
@@ -62,18 +58,14 @@ class _LoginPageState extends State<LoginPage> {
           SingleChildScrollView(
               child: Container(
                   constraints: BoxConstraints(
-                      minHeight: MediaQuery.of(context).size.height -
-                          MediaQuery.of(context).viewInsets.bottom),
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        LoginUI(
-                          authenticationBloc: _authenticationBloc,
-                          loginBloc: _loginBloc,
-                          userService: _userService,
-                        )
-                      ]))),
+                      minHeight: MediaQuery.of(context).size.height - MediaQuery.of(context).viewInsets.bottom),
+                  child: Column(mainAxisAlignment: MainAxisAlignment.center, mainAxisSize: MainAxisSize.max, children: [
+                    LoginUI(
+                      authenticationBloc: _authenticationBloc,
+                      loginBloc: _loginBloc,
+                      userService: _userService,
+                    )
+                  ]))),
         ]));
   }
 
