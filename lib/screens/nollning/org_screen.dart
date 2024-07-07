4cfeb97efafb0effb9513cfeb3c4758f862976d0
Trobbi <samueltrobbiani02@gmail.com>
2024-07-08 01:24:26 +0200

    Decent grafik men fett lökig grafik, hur fan gör man saker scrollable?????

--- lib/screens/nollning/org_screen.dart
+++ lib/screens/nollning/org_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:fsek_mobile/april_fools.dart';
+import 'package:photo_view/photo_view.dart';
 
 class OrganizationScreenPage extends StatefulWidget {
   @override
@@ -17,17 +18,21 @@ class _OrganizationScreenState extends State<OrganizationScreenPage> {
     String backgroundPaperPath = "assets/img/nollning-24/organization_tree/orgscreen_paper.png";
     String backgroundWoodPath = "assets/img/nollning-24/organization_tree/orgscreen_wood_background.png";
     String tlthButtonPath = "assets/img/nollning-24/organization_tree/orgscreen_tlth.png";
+    String orgTextButtonPath = "assets/img/nollning-24/organization_tree/orgscreen_textbutton.png";
+    String fsekButtonPath = "assets/img/nollning-24/organization_tree/orgscreen_fsek.png";
+    String orgFortPath = "assets/img/nollning-24/organization_tree/orgscreen_fortdesign.png";
+
+/*
+    return Stack(children: [SingleChildScrollView(
+      child: Column(children: [PhotoView(imageProvider: AssetImage(backgroundWoodPath))],
+
+      )
+    )]);
+  }
+}*/
+
     
     return Stack(children: [
-      Scaffold(
-      backgroundColor: Colors.transparent,
-      appBar: AppBar(
-        title: Text(
-          "Organisationsträd",
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
-      ),
-    ),
       Image.asset(
         backgroundWoodPath,
         height: MediaQuery.of(context).size.height,
@@ -40,15 +45,161 @@ class _OrganizationScreenState extends State<OrganizationScreenPage> {
         width: MediaQuery.of(context).size.width,
         fit: BoxFit.cover,
         alignment: Alignment.topCenter,),
+      Padding(padding: EdgeInsets.only(top: 80), child: Container(
+        alignment: Alignment.topCenter,
+        child: Image.asset(
+          tlthButtonPath,
+          height: MediaQuery.of(context).size.height/1.5,
+          width: MediaQuery.of(context).size.width/1.5,
+          fit: BoxFit.cover,
+          alignment: Alignment.topCenter,),)),
+      Padding(padding: EdgeInsets.only(top: 160, left: 30), child: Container(alignment: Alignment.topCenter, 
+      child: Row(
+        children: [Stack(children: [
+          Image.asset(
+            orgTextButtonPath, 
+            height: MediaQuery.of(context).size.height/2.3, 
+            width: MediaQuery.of(context).size.width/2.3,
+            fit: BoxFit.cover,
+            alignment: Alignment.topCenter), 
+          FittedBox(fit: BoxFit.contain, child: Text("Organisation", style: Theme.of(context).textTheme.headlineMedium))]), 
+          Stack(children: [Image.asset(
+              orgTextButtonPath, 
+              height: MediaQuery.of(context).size.height/2.3, 
+              width: MediaQuery.of(context).size.width/2.3,
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter), 
+          Padding(padding: EdgeInsets.only(top: 7, left: 40), child: Text("Sektioner", style: Theme.of(context).textTheme.headlineMedium))])],))),
+          Padding(padding: EdgeInsets.only(top: 150), child: Container(alignment: Alignment.center, 
+            child: Image.asset(
+              fsekButtonPath,
+              height: MediaQuery.of(context).size.height/1.5,
+              width: MediaQuery.of(context).size.width/1.5,
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter,))),
+          Padding(padding: EdgeInsets.only(top: 110, left: 30), child: Container(alignment: Alignment.center, child: Row(children: [Stack(children: [Image.asset(
+              orgTextButtonPath, 
+              height: MediaQuery.of(context).size.height/2.3, 
+              width: MediaQuery.of(context).size.width/2.3,
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter), Text("", style: Theme.of(context).textTheme.headlineMedium)]), 
+            Stack(children: [Image.asset(
+              orgTextButtonPath, 
+              height: MediaQuery.of(context).size.height/2.3, 
+              width: MediaQuery.of(context).size.width/2.3,
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter), Text("", style: Theme.of(context).textTheme.headlineMedium)])]))),
+          Padding(padding: EdgeInsets.only(top: 210, left: 30), child: Container(alignment: Alignment.center, child: Row(children: [Stack(children: [Image.asset(
+              orgTextButtonPath, 
+              height: MediaQuery.of(context).size.height/2.3, 
+              width: MediaQuery.of(context).size.width/2.3,
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter), Text("", style: Theme.of(context).textTheme.headlineMedium)]), 
+            Stack(children: [Image.asset(
+              orgTextButtonPath, 
+              height: MediaQuery.of(context).size.height/2.3, 
+              width: MediaQuery.of(context).size.width/2.3,
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter), Text("", style: Theme.of(context).textTheme.headlineMedium)])]))),
+          Padding(padding: EdgeInsets.only(top: 310, left: 30), child: Container(alignment: Alignment.center, child: Row(children: [Stack(children: [Image.asset(
+              orgTextButtonPath, 
+              height: MediaQuery.of(context).size.height/2.3, 
+              width: MediaQuery.of(context).size.width/2.3,
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter), Text("", style: Theme.of(context).textTheme.headlineMedium)]), 
+            Stack(children: [Image.asset(
+              orgTextButtonPath, 
+              height: MediaQuery.of(context).size.height/2.3, 
+              width: MediaQuery.of(context).size.width/2.3,
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter), Text("", style: Theme.of(context).textTheme.headlineMedium)])]))),
+          Padding(padding: EdgeInsets.only(top: 410, left: 30), child: Container(alignment: Alignment.center, child: Row(children: [Stack(children: [Image.asset(
+              orgTextButtonPath, 
+              height: MediaQuery.of(context).size.height/2.3, 
+              width: MediaQuery.of(context).size.width/2.3,
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter), Text("", style: Theme.of(context).textTheme.headlineMedium)]), 
+            Stack(children: [Image.asset(
+              orgTextButtonPath, 
+              height: MediaQuery.of(context).size.height/2.3, 
+              width: MediaQuery.of(context).size.width/2.3,
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter), Text("", style: Theme.of(context).textTheme.headlineMedium)])]))),
+          Padding(padding: EdgeInsets.only(top: 510, left: 30), child: Container(alignment: Alignment.center, child: Row(children: [Stack(children: [Image.asset(
+              orgTextButtonPath, 
+              height: MediaQuery.of(context).size.height/2.3, 
+              width: MediaQuery.of(context).size.width/2.3,
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter), Text("", style: Theme.of(context).textTheme.headlineMedium)]), 
+            Stack(children: [Image.asset(
+              orgTextButtonPath, 
+              height: MediaQuery.of(context).size.height/2.3, 
+              width: MediaQuery.of(context).size.width/2.3,
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter), Text("", style: Theme.of(context).textTheme.headlineMedium)])]))),
+          Padding(padding: EdgeInsets.only(top: 575, left: 30), child: Container(alignment: Alignment.center, child: Row(children: [Stack(children: [Image.asset(
+              orgTextButtonPath, 
+              height: MediaQuery.of(context).size.height/2.3, 
+              width: MediaQuery.of(context).size.width/2.3,
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter), Text("", style: Theme.of(context).textTheme.headlineMedium)]), 
+            Stack(children: [Image.asset(
+              orgTextButtonPath, 
+              height: MediaQuery.of(context).size.height/2.3, 
+              width: MediaQuery.of(context).size.width/2.3,
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter), Text("", style: Theme.of(context).textTheme.headlineMedium)])]))),
+          Padding(padding: EdgeInsets.only(top: 630), child: Container(
+            alignment: Alignment.topCenter,
+            child: Stack(children: [Image.asset(
+              orgTextButtonPath,
+              height: MediaQuery.of(context).size.height/2.3,
+              width: MediaQuery.of(context).size.width/2.3,
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter,), 
+              Text("", style: Theme.of(context).textTheme.headlineMedium)]))),  
+          Padding(padding: EdgeInsets.only(top: 750, left: 30), child: Container(alignment: Alignment.center, child: Row(children: [Stack(children: [Image.asset(
+              orgTextButtonPath, 
+              height: MediaQuery.of(context).size.height/2.3, 
+              width: MediaQuery.of(context).size.width/2.3,
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter), Text("", style: Theme.of(context).textTheme.headlineMedium)]), 
+            Stack(children: [Image.asset(
+              orgTextButtonPath, 
+              height: MediaQuery.of(context).size.height/2.3, 
+              width: MediaQuery.of(context).size.width/2.3,
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter), Text("", style: Theme.of(context).textTheme.headlineMedium)])]))),
+          Padding(padding: EdgeInsets.only(top: 805), child: Container(
+            alignment: Alignment.topCenter,
+            child: Stack(children: [Image.asset(
+              orgTextButtonPath,
+              height: MediaQuery.of(context).size.height/2.3,
+              width: MediaQuery.of(context).size.width/2.3,
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter,), 
+              Text("", style: Theme.of(context).textTheme.headlineMedium)]))),
+          Padding(padding: EdgeInsets.only(top: 50), child: Container(
+            alignment: Alignment.topCenter,
+            child: Image.asset(
+              orgFortPath,
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter
+            ))),
       Scaffold(
       backgroundColor: Colors.transparent,
       appBar: AppBar(
-        title: Text(
-          "Organisationsträd",
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
+        backgroundColor: Colors.transparent, // Color(0xFF7E5127),
+        //title: Text(
+          //"Organisationsträd",
+          //style: Theme.of(context).textTheme.headlineMedium,
+        //),
       ),
     ),
     ]);
   }
 }
+
+
