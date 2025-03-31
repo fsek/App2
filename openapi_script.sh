21ebf2a0014674e77dc8891b87af84a5172dee1f
Benjamin Halasz <benjamin_halasz@icloud.com>
2025-03-31 18:07:21 +0200

    some more cleanup

--- openapi_script.sh
+++ openapi_script.sh
@@ -8,13 +8,8 @@ openapi-generator generate \
   -i "$temp_spec" \
   -g dart-dio \
   -o lib/api_client \
-  --additional-properties=pubName=api_client,baseUrl=http://localhost:8080
+  --additional-properties=pubName=api_client,baseUrl=http://localhost:8000
 
 rm "$temp_spec"
 
 
-# openapi-generator generate \
-#   -i path/to/openapi.json \
-#   -g dart-dio \
-#   -o lib/api_client \
-#   --additional-properties=pubName=api_client,baseUrl=http://localhost:8080
