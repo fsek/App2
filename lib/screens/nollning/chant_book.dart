209c9af3b8f18823c675361ee9a8c7059cbbf3a0
s4mp <samueltrobbiani02@gmail.com>
2025-04-15 00:35:20 +0200

    wtf am i doing

--- lib/screens/nollning/chant_book.dart
+++ lib/screens/nollning/chant_book.dart
@@ -1,164 +1,167 @@
-import 'package:flutter/material.dart';
-import 'package:fsek_mobile/models/songbook/songbookEntry.dart';
-import 'package:fsek_mobile/screens/songbook/song.dart';
-import 'package:fsek_mobile/services/service_locator.dart';
-import 'package:fsek_mobile/services/song.service.dart';
-import 'package:fsek_mobile/services/songbook.service.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+// import 'package:flutter/material.dart';
+// import 'package:fsek_mobile/models/songbook/songbookEntry.dart';
+// import 'package:fsek_mobile/screens/songbook/song.dart';
+// import 'package:fsek_mobile/services/service_locator.dart';
+// import 'package:fsek_mobile/services/song.service.dart';
+// import 'package:fsek_mobile/services/songbook.service.dart';
+// import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class ChantBookPage extends StatefulWidget {
-  @override
-  _ChantBookPageState createState() => _ChantBookPageState();
-}
+// class ChantBookPage extends StatefulWidget {
+//   @override
+//   _ChantBookPageState createState() => _ChantBookPageState();
+// }
 
-class _ChantBookPageState extends State<ChantBookPage> {
-  List<SongbookEntry> chants = [];
-  List<SongbookEntry> allChants = [];
-  TextEditingController _controller = TextEditingController();
-  bool searchFocus = false;
-  String chantAuthor = "";
+// class _ChantBookPageState extends State<ChantBookPage> {
+//   List<SongbookEntry> chants = [];
+//   List<SongbookEntry> allChants = [];
+//   TextEditingController _controller = TextEditingController();
+//   bool searchFocus = false;
+//   String chantAuthor = "";
 
-  void initState() {
-    locator<SongbookService>().getChantbook().then((value) => setState(() {
-          this.chants = value;
-          chants.sort((a, b) {
-            if (a.author!.compareTo(b.author!) != 0)
-              return a.author!.compareTo(b.author!);
-            else
-              return a.title!.compareTo(b.title!);
-          }); // handle null?
-          allChants = List.from(chants);
-        }));
-    super.initState();
-  }
+//   void initState() {
+//     locator<SongbookService>().getChantbook().then((value) => setState(() {
+//           this.chants = value;
+//           chants.sort((a, b) {
+//             if (a.author!.compareTo(b.author!) != 0)
+//               return a.author!.compareTo(b.author!);
+//             else
+//               return a.title!.compareTo(b.title!);
+//           }); // handle null?
+//           allChants = List.from(chants);
+//         }));
+//     super.initState();
+//   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
 
-  Widget build(BuildContext context) {
-    var t = AppLocalizations.of(context)!;
-    return allChants == []
-        ? Scaffold(
-            appBar: AppBar(title: Text(t.introductionGuildChants)),
-            body: Center(
-                child: CircularProgressIndicator(
-                    color: Theme.of(context).colorScheme.primary)))
-        : Scaffold(
-            appBar: AppBar(title: Text(t.introductionGuildChants)),
-            body: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                FocusScope(
-                    //Unfocusing from the searchbar from any tap would be nice but it is minor
-                    child: Focus(
-                  onFocusChange: (focus) {
-                    setState(() {
-                      searchFocus = focus;
-                    });
-                  },
-                  child: TextField(
-                    controller: _controller,
-                    decoration: InputDecoration(
-                        prefixIcon: searchFocus
-                            ? IconButton(
-                                icon: Icon(
-                                  Icons.arrow_back,
-                                  color: Theme.of(context).colorScheme.onSurface,
-                                ),
-                                onPressed: () =>
-                                    FocusScope.of(context).unfocus())
-                            : Icon(
-                                Icons.search,
-                                color: Theme.of(context).colorScheme.onSurface,
-                              ),
-                        hintText: t.introductionSearch,
-                        suffixIcon: _controller.text.length > 0
-                            ? IconButton(
-                                icon: Icon(Icons.clear),
-                                color: Theme.of(context).colorScheme.onSurface,
-                                onPressed: () => setState(() {
-                                      _controller.clear();
-                                      FocusScope.of(context).unfocus();
-                                      chants = allChants;
-                                    }))
-                            : SizedBox.shrink()),
-                    onChanged: (search) {
-                      List<String> searchTerms =
-                          search.toLowerCase().trim().split(new RegExp(r"\s+"));
-                      setState(() {
-                        chantAuthor = "";
-                        chants = allChants.where((song) {
-                          return searchTerms.every((term) =>
-                              song.title!.toLowerCase().contains(term));
-                        }).toList();
-                      });
-                    },
-                  ),
-                )),
-                Expanded(
-                  child: chants.isNotEmpty
-                      ? ListView(
-                          children: chants
-                              .map((song) => _generateSongTile(song))
-                              .toList(),
-                        )
-                      : Padding(
-                          padding: EdgeInsets.all(16),
-                          child: Text(t.introductionNoMatches),
-                        ),
-                )
-              ],
-            ),
-            floatingActionButton: FloatingActionButton.extended(
-              onPressed: () {
-                Navigator.pushNamed(context, '/song_book');
-              },
-              label: Text(t.introductionEntireSongBook),
-              icon: const Icon(Icons.queue_music),
-              backgroundColor: Color(0xffc0a835),
-            ),
-          );
-  }
+//   Widget build(BuildContext context) {
+//     var t = AppLocalizations.of(context)!;
+//     return allChants == []
+//         ? Scaffold(
+//             appBar: AppBar(title: Text(t.introductionGuildChants)),
+//             body: Center(
+//                 child: CircularProgressIndicator(
+//                     color: Theme.of(context).colorScheme.primary)))
+//         : Scaffold(
+//             appBar: AppBar(title: Text(t.introductionGuildChants)),
+//             body: Column(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 FocusScope(
+//                     //Unfocusing from the searchbar from any tap would be nice but it is minor
+//                     child: Focus(
+//                   onFocusChange: (focus) {
+//                     setState(() {
+//                       searchFocus = focus;
+//                     });
+//                   },
+//                   child: TextField(
+//                     controller: _controller,
+//                     decoration: InputDecoration(
+//                         prefixIcon: searchFocus
+//                             ? IconButton(
+//                                 icon: Icon(
+//                                   Icons.arrow_back,
+//                                   color:
+//                                       Theme.of(context).colorScheme.onSurface,
+//                                 ),
+//                                 onPressed: () =>
+//                                     FocusScope.of(context).unfocus())
+//                             : Icon(
+//                                 Icons.search,
+//                                 color: Theme.of(context).colorScheme.onSurface,
+//                               ),
+//                         hintText: t.introductionSearch,
+//                         suffixIcon: _controller.text.length > 0
+//                             ? IconButton(
+//                                 icon: Icon(Icons.clear),
+//                                 color: Theme.of(context).colorScheme.onSurface,
+//                                 onPressed: () => setState(() {
+//                                       _controller.clear();
+//                                       FocusScope.of(context).unfocus();
+//                                       chants = allChants;
+//                                     }))
+//                             : SizedBox.shrink()),
+//                     onChanged: (search) {
+//                       List<String> searchTerms =
+//                           search.toLowerCase().trim().split(new RegExp(r"\s+"));
+//                       setState(() {
+//                         chantAuthor = "";
+//                         chants = allChants.where((song) {
+//                           return searchTerms.every((term) =>
+//                               song.title!.toLowerCase().contains(term));
+//                         }).toList();
+//                       });
+//                     },
+//                   ),
+//                 )),
+//                 Expanded(
+//                   child: chants.isNotEmpty
+//                       ? ListView(
+//                           children: chants
+//                               .map((song) => _generateSongTile(song))
+//                               .toList(),
+//                         )
+//                       : Padding(
+//                           padding: EdgeInsets.all(16),
+//                           child: Text(t.introductionNoMatches),
+//                         ),
+//                 )
+//               ],
+//             ),
+//             floatingActionButton: FloatingActionButton.extended(
+//               onPressed: () {
+//                 Navigator.pushNamed(context, '/song_book');
+//               },
+//               label: Text(t.introductionEntireSongBook),
+//               icon: const Icon(Icons.queue_music),
+//               backgroundColor: Color(0xffc0a835),
+//             ),
+//           );
+//   }
 
-  Widget _generateSongTile(SongbookEntry song) {
-    //This way of doing it is probably really stupid. but so be it
-    List<Widget> index = [];
-    if (song.author != chantAuthor) {
-      chantAuthor = song.author!;
-      index.add(Container(
-        decoration: BoxDecoration(color: Theme.of(context).colorScheme.surfaceVariant),
-        child: ListTile(
-          title: Text(
-            chantAuthor,
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-        ),
-      ));
-    }
-    return Column(
-      children: index +
-          [
-            Container(
-                decoration: BoxDecoration(
-                    border: Border(
-                  bottom: BorderSide(color: Theme.of(context).colorScheme.onSurfaceVariant),
-                )),
-                child: InkWell(
-                  onTap: () => openSong(song.id!),
-                  child: ListTile(
-                      title: Text(song.title == null ? "" : song.title!)),
-                ))
-          ],
-    );
-  }
+//   Widget _generateSongTile(SongbookEntry song) {
+//     //This way of doing it is probably really stupid. but so be it
+//     List<Widget> index = [];
+//     if (song.author != chantAuthor) {
+//       chantAuthor = song.author!;
+//       index.add(Container(
+//         decoration:
+//             BoxDecoration(color: Theme.of(context).colorScheme.surfaceVariant),
+//         child: ListTile(
+//           title: Text(
+//             chantAuthor,
+//             style: TextStyle(fontWeight: FontWeight.bold),
+//           ),
+//         ),
+//       ));
+//     }
+//     return Column(
+//       children: index +
+//           [
+//             Container(
+//                 decoration: BoxDecoration(
+//                     border: Border(
+//                   bottom: BorderSide(
+//                       color: Theme.of(context).colorScheme.onSurfaceVariant),
+//                 )),
+//                 child: InkWell(
+//                   onTap: () => openSong(song.id!),
+//                   child: ListTile(
+//                       title: Text(song.title == null ? "" : song.title!)),
+//                 ))
+//           ],
+//     );
+//   }
 
-  void openSong(int id) {
-    locator<SongService>().getSong(id).then((song) {
-      Navigator.push(context,
-          MaterialPageRoute(builder: (context) => SongPage(song: song)));
-    });
-  }
-}
+//   void openSong(int id) {
+//     locator<SongService>().getSong(id).then((song) {
+//       Navigator.push(context,
+//           MaterialPageRoute(builder: (context) => SongPage(song: song)));
+//     });
+//   }
+// }
--- lib/screens/songbook/top_songs.dart
+++ lib/screens/songbook/top_songs.dart
@@ -1,132 +1,142 @@
-import 'package:flutter/material.dart';
-import 'package:fsek_mobile/models/songbook/songbookEntry.dart';
-import 'package:fsek_mobile/screens/songbook/song.dart';
-import 'package:fsek_mobile/services/service_locator.dart';
-import 'package:fsek_mobile/services/song.service.dart';
-import 'package:fsek_mobile/services/songbook.service.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+// import 'package:flutter/material.dart';
+// import 'package:fsek_mobile/models/songbook/songbookEntry.dart';
+// import 'package:fsek_mobile/screens/songbook/song.dart';
+// import 'package:fsek_mobile/services/service_locator.dart';
+// import 'package:fsek_mobile/services/song.service.dart';
+// import 'package:fsek_mobile/services/songbook.service.dart';
+// import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class TopSongsPage extends StatefulWidget {
-  @override
-  _TopSongsPageState createState() => _TopSongsPageState();
-}
+// class TopSongsPage extends StatefulWidget {
+//   @override
+//   _TopSongsPageState createState() => _TopSongsPageState();
+// }
 
-class _TopSongsPageState extends State<TopSongsPage> with TickerProviderStateMixin {
-  List<SongbookEntry> songs = [];
-  List<SongbookEntry> TopSongs = [];
+// class _TopSongsPageState extends State<TopSongsPage>
+//     with TickerProviderStateMixin {
+//   List<SongbookEntry> songs = [];
+//   List<SongbookEntry> TopSongs = [];
 
-  //bad helpvariables that are most likely unneeded
-  bool searchFocus = false;
-  late int initChar;
+//   //bad helpvariables that are most likely unneeded
+//   bool searchFocus = false;
+//   late int initChar;
 
-  TextEditingController _controller = TextEditingController();
+//   TextEditingController _controller = TextEditingController();
 
-  late AnimationController animationController;
-  late Animation<double> animation;
+//   late AnimationController animationController;
+//   late Animation<double> animation;
 
-  @override
-  void initState() {
-    int num_top_songs = 10;
-    animationController = AnimationController(vsync: this, duration: Duration(milliseconds: 1400));
-    locator<SongbookService>().getTopSongs(num_top_songs).then((value) => setState(() {
-          this.songs = value;
-          TopSongs = List.from(songs);
-        }));
+//   @override
+//   void initState() {
+//     int num_top_songs = 10;
+//     animationController = AnimationController(
+//         vsync: this, duration: Duration(milliseconds: 1400));
+//     locator<SongbookService>()
+//         .getTopSongs(num_top_songs)
+//         .then((value) => setState(() {
+//               this.songs = value;
+//               TopSongs = List.from(songs);
+//             }));
 
-    super.initState();
-  }
+//     super.initState();
+//   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
 
+//   Widget build(BuildContext context) {
+//     var t = AppLocalizations.of(context)!;
+//     initChar = 1;
+//     return TopSongs == []
+//         ? Scaffold(
+//             //change text
+//             appBar: AppBar(title: Text(t.songbookSongbook)),
+//             body: Center(
+//                 child: CircularProgressIndicator(
+//                     color: Theme.of(context).primaryColor)))
+//         : GestureDetector(
+//             onTap: () => FocusScope.of(context).unfocus(),
+//             child: Scaffold(
+//               appBar: AppBar(
+//                 title: Text(t.songbookSongbook),
+//               ),
+//               body: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   Expanded(
+//                     child: songs.isNotEmpty
+//                         ? ListView(
+//                             children: songs
+//                                 .map((song) => _generateSongTile(song))
+//                                 .toList(),
+//                           )
+//                         : Padding(
+//                             padding: EdgeInsets.all(16),
+//                             child: Text(t.songbookNoMatches),
+//                           ),
+//                   )
+//                 ],
+//               ),
+//             ),
+//           );
+//   }
 
-  Widget build(BuildContext context) {
-    var t = AppLocalizations.of(context)!;
-    initChar = 1;
-    return TopSongs == []
-        ? Scaffold(
-            //change text
-            appBar: AppBar(title: Text(t.songbookSongbook)),
-            body: Center(child: CircularProgressIndicator(color: Theme.of(context).primaryColor)))
-        : GestureDetector(
-            onTap: () => FocusScope.of(context).unfocus(),
-            child: Scaffold(
-                  appBar: AppBar(
-                    title: Text(t.songbookSongbook),
-                  ),
-                  body: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Expanded(
-                        child: songs.isNotEmpty
-                            ? ListView(
-                                children: songs.map((song) => _generateSongTile(song)).toList(),
-                              )
-                            : Padding(
-                                padding: EdgeInsets.all(16),
-                                child: Text(t.songbookNoMatches),
-                              ),
-                      )
-                    ],
-                  ),
-                ),
-              );
-  }
+//   Widget _generateSongTile(SongbookEntry song) {
+//     Color color;
+//     Color numColor;
 
-  Widget _generateSongTile(SongbookEntry song) {
-    Color color;
-    Color numColor;
+//     List<Widget> index = [];
+//     //deciding colour
+//     if (initChar == 1)
+//       color = Color.fromARGB(255, 255, 215, 0);
+//     else if (initChar == 2)
+//       color = Color.fromARGB(255, 192, 192, 192);
+//     else if (initChar == 3)
+//       color = Color.fromARGB(255, 205, 127, 50);
+//     else
+//       color = Theme.of(context).colorScheme.surfaceVariant;
 
-    List<Widget> index = [];
-    //deciding colour
-    if (initChar == 1)
-      color = Color.fromARGB(255, 255, 215, 0);
-    else if (initChar == 2)
-      color = Color.fromARGB(255, 192, 192, 192);
-    else if (initChar == 3)
-      color = Color.fromARGB(255, 205, 127, 50);
-    else
-      color = Theme.of(context).colorScheme.surfaceVariant;
+//     // Same for the number
+//     if (initChar <= 3)
+//       numColor = Color.fromARGB(255, 0, 0, 0);
+//     else
+//       numColor = Theme.of(context).colorScheme.onSurfaceVariant;
 
-    // Same for the number
-    if (initChar <= 3)
-      numColor = Color.fromARGB(255, 0, 0, 0);
-    else
-      numColor = Theme.of(context).colorScheme.onSurfaceVariant;
+//     index.add(Container(
+//       decoration: BoxDecoration(color: color),
+//       child: ListTile(
+//         title: Text(
+//           initChar.toString(),
+//           style: TextStyle(fontWeight: FontWeight.bold, color: numColor),
+//         ),
+//       ),
+//     ));
+//     initChar += 1;
 
-    index.add(Container(
-      decoration: BoxDecoration(color: color),
-      child: ListTile(
-        title: Text(
-          initChar.toString(),
-          style: TextStyle(fontWeight: FontWeight.bold, color: numColor),
-        ),
-      ),
-    ));
-    initChar += 1;
+//     return Column(
+//       children: index +
+//           [
+//             Container(
+//                 decoration: BoxDecoration(
+//                     border: Border(
+//                   bottom: BorderSide(
+//                       color: Theme.of(context).colorScheme.surfaceVariant),
+//                 )),
+//                 child: InkWell(
+//                   onTap: () => openSong(song.id!),
+//                   child: ListTile(
+//                       title: Text(song.title == null ? "" : song.title!)),
+//                 ))
+//           ],
+//     );
+//   }
 
-    return Column(
-      children: index +
-          [
-            Container(
-                decoration: BoxDecoration(
-                    border: Border(
-                  bottom: BorderSide(color: Theme.of(context).colorScheme.surfaceVariant),
-                )),
-                child: InkWell(
-                  onTap: () => openSong(song.id!),
-                  child: ListTile(title: Text(song.title == null ? "" : song.title!)),
-                ))
-          ],
-    );
-  }
-
-  void openSong(int id) {
-    locator<SongService>().getSong(id).then((song) {
-      Navigator.push(context, MaterialPageRoute(builder: (context) => SongPage(song: song)));
-    });
-  }
-}
+//   void openSong(int id) {
+//     locator<SongService>().getSong(id).then((song) {
+//       Navigator.push(context,
+//           MaterialPageRoute(builder: (context) => SongPage(song: song)));
+//     });
+//   }
+// }
