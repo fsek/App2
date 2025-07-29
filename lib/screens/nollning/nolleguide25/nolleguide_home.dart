f16190fd85d3512062f325aa0c187ee3acb97b18
trobbi <samueltrobbiani02@gmail.com>
2025-07-29 19:28:17 +0200

    added the assets to do homescreen but missing some images

---
+++ lib/screens/nollning/nolleguide25/nolleguide_home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:fsek_mobile/screens/nollning/nolleguide-24/kladguide.dart';
+import 'package:fsek_mobile/screens/nollning/nolleguide-24/stodfunktioner.dart';
+import 'package:fsek_mobile/screens/nollning/nolleguide-24/wordlist.dart';
+import 'package:fsek_mobile/screens/nollning/nolleguide-24/orgscreen/org_screen.dart';
+import 'package:fsek_mobile/screens/nollning/nolleguide-24/studentvett.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:fsek_mobile/services/preload_asset.service.dart';
+
+class NolleGuideHomePage extends StatefulWidget {
+  @override
+  _NolleGuideHomeState createState() => _NolleGuideHomeState();
+}
+
+class _NolleGuideHomeState extends State<NolleGuideHomePage> {
+  void initState() {
+    super.initState();
+  }
+
+
+}
