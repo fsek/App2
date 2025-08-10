2d941d0ebf66cf095cdc02e9ff7197cb93a3425b
s4mp <samueltrobbiani02@gmail.com>
2025-08-11 01:37:00 +0200

    added comment in script

--- script.sh
+++ script.sh
@@ -22,7 +22,7 @@ sed -i "/^environment:/,/^[^[:space:]]/ s/^\([[:space:]]*sdk:[[:space:]]*\).*/\1
 echo "Done!"
 
 
-# After running the script, you also need to change the lower version lock of flutter in pubspec.yaml to 3.0.0
+# After running the script, you also need to change the lower version lock of flutter in pubspec.yaml to 3.0.0 (this is done in the sed command now! But you might want to double check the version)
 # For usage from emulator, use http://10.0.2.2 instead of localhost in api.dart.
 # We also might need to change pubspec.yaml it should be sdk: '>=3.0.0 <4.0.0'.
 # command for running in windows cmd with -cli using openapi.json file in App2 folder:
