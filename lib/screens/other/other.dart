b31b1964153b9f734402db8d8cc91340afda9f1a
Markus Pettersson <osmarkusp@gmail.com>
2026-05-16 11:41:18 +0200

    Re-add nolleguide to the "other" tab

--- lib/screens/other/other.dart
+++ lib/screens/other/other.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fsek_mobile/screens/contact/contact.dart';
 import 'package:fsek_mobile/screens/gallery/gallery.dart';
 import 'package:fsek_mobile/screens/nollning/emergency_contacts.dart';
+import 'package:fsek_mobile/screens/nollning/nolleguide_26/nolleguide_home.dart';
 import 'package:fsek_mobile/screens/other/aboutGuild.dart';
 import 'package:fsek_mobile/screens/settings/language_settings.dart';
 import 'package:fsek_mobile/screens/settings/settings.dart';
@@ -33,7 +34,7 @@ class OtherContent extends StatelessWidget {
       t.otherSongbook,
       t.otherGallery,
       t.otherCafe,
-      //t.nolleguide,
+      t.nolleguide,
       t.emergencyContacts
     ];
     about = [t.otherAboutGuild, t.otherFap];
@@ -45,8 +46,8 @@ class OtherContent extends StatelessWidget {
       "Songbook": SongbookPage(),
       "Photo Gallery": GalleryPage(),
       "Hilbert Café": CafePage(),
-      //"Nolleguiden": NolleGuideHomePage(),
-      //"The Student Guide": NolleGuideHomePage(),
+      "Nolleguiden": NolleGuideHomePage(),
+      "The Student Guide": NolleGuideHomePage(),
       "Nödkontaktnummer": EmergencyContactsPage(),
       "Emergency Contact Numbers": EmergencyContactsPage(),
       "The F guild": AboutGuildPage(),
