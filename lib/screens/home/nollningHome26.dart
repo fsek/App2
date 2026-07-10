a07c685e69ef890373397ca03712f1ad7547297f
Markus Pettersson <osmarkusp@gmail.com>
2026-07-10 12:19:28 +0200

    Basic homescreen logic

--- lib/screens/home/nollningHome26.dart
+++ lib/screens/home/nollningHome26.dart
@@ -6,6 +6,7 @@ import 'package:fsek_mobile/screens/nollning/nolleguide_26/nolleguide_home.dart'
 import 'package:fsek_mobile/screens/nollning/questscreen_26/quest_home.dart';
 import 'package:fsek_mobile/screens/nollning/schedule.dart';
 import 'package:fsek_mobile/screens/songbook/songbook.dart';
+import 'package:fsek_mobile/util/nollning/week_tracker.dart';
 
 class NollningHomePage extends StatefulWidget {
   static const routeName = '/homepage';
@@ -22,6 +23,7 @@ class _NollningHomePageState extends State<NollningHomePage> {
 
   Widget build(BuildContext context) {
     var t = AppLocalizations.of(context)!;
+    var week = WeekTracker.determineWeek();
 
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
@@ -30,13 +32,19 @@ class _NollningHomePageState extends State<NollningHomePage> {
     const double buttonSpace = 30; // The space between the buttons
     const double bottomMargin = 30; // The margin from the navbar
 
-    const String backgroundImage = "assets/data/nollning_26/homescreen/background.png";
-    const String buttonImage = "assets/data/nollning_26/homescreen/button.png";
+    var filePath = 'assets/data/nollning_26/homescreen/lvl_${week}';
+
+    var backgroundImage = '${filePath}/background.png';
+    var contactButton = '${filePath}/button_contact_${t.localeName}.png';
+    var mapButton = '${filePath}/button_map_${t.localeName}';
+    var guideButton = '${filePath}/button_guide_${t.localeName}.png';
+    var scheduleButton = '${filePath}/button_schedule_${t.localeName}.png';
+    var questButton = '${filePath}/button_quest_${t.localeName}.png';
+    var songButton = '${filePath}/button_song_${t.localeName}.png';
 
     return Container(
       child: Stack(children: [
         Positioned.fill(child: Image.asset(backgroundImage, fit: BoxFit.fill)),
-        Align(alignment: Alignment.topCenter, child: Text("En nollning på ny nivå!", style: TextStyle(color: Colors.white),),),
         Align(alignment: Alignment.bottomCenter, child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.end,
@@ -49,7 +57,7 @@ class _NollningHomePageState extends State<NollningHomePage> {
                     width: screenWidth / buttonSize,
                     child: InkWell(
                       onTap: () => (Navigator.push(context, MaterialPageRoute(builder: (context) => NolleGuideHomePage()))),
-                      child: generateButton(buttonImage, "Nolle-\nguiden"),
+                      child: Image.asset(guideButton)
                     ),
                   ),
                   SizedBox(
@@ -59,7 +67,7 @@ class _NollningHomePageState extends State<NollningHomePage> {
                     width: screenWidth / buttonSize,
                     child: InkWell(
                       onTap: () => (Navigator.push(context, MaterialPageRoute(builder: (context) => ScheduleScreenPage()))),
-                      child: generateButton(buttonImage, "Schema"),
+                      child: Image.asset(scheduleButton)
                     ),
                   )
                 ],
@@ -73,7 +81,7 @@ class _NollningHomePageState extends State<NollningHomePage> {
                     width: screenWidth / buttonSize,
                     child: InkWell(
                       onTap: () => (Navigator.push(context, MaterialPageRoute(builder: (context) => EmergencyContactsPage()))),
-                      child: generateButton(buttonImage, "Kontakt"),
+                      child: Image.asset(contactButton)
                     ),
                   ),
                   SizedBox(
@@ -83,7 +91,7 @@ class _NollningHomePageState extends State<NollningHomePage> {
                     width: screenWidth / buttonSize,
                     child: InkWell(
                       onTap: () => (Navigator.push(context, MaterialPageRoute(builder: (context) => QuestHomeScreen()))),
-                      child: generateButton(buttonImage, "Side\nQuest"),
+                      child: Image.asset(questButton)
                     ),
                   )
                 ],
@@ -97,7 +105,7 @@ class _NollningHomePageState extends State<NollningHomePage> {
                     width: screenWidth / buttonSize,
                     child: InkWell(
                       onTap: () => (Navigator.push(context, MaterialPageRoute(builder: (context) => SongbookPage()))),
-                      child: generateButton(buttonImage, "Sångbok"),
+                      child: Image.asset(songButton)
                     ),
                   ),
                   SizedBox(
@@ -107,7 +115,7 @@ class _NollningHomePageState extends State<NollningHomePage> {
                     width: screenWidth / buttonSize,
                     child: InkWell(
                       onTap: () => (Navigator.push(context, MaterialPageRoute(builder: (context) => MapView()))),
-                      child: generateButton(buttonImage, "Karta"),
+                      child: Image.asset(mapButton)
                     ),
                   )
                 ],
