3712a8b338f3671a58950608eb841eecb111a2af
Sam Dreyer <sam.sol.dreyer@gmail.com>
2026-08-17 19:38:32 +0300

    Committing the work so far. This is the new quest screen for nollning 2026 including leaderboard.

---
+++ lib/screens/nollning/questscreen_26/highscore.dart
@@ -0,0 +1,221 @@
+import 'package:flutter/material.dart';
+import 'package:fsek_mobile/api_client/lib/api_client.dart';
+import 'package:fsek_mobile/services/api.service.dart';
+import 'package:fsek_mobile/l10n/app_localizations.dart';
+
+class HighscoreScreen extends StatefulWidget {
+  final double availableWidth;
+  final double availableHeight;
+
+  HighscoreScreen({
+    required this.availableHeight,
+    required this.availableWidth,
+  });
+
+  @override
+  _HighscoreScreenState createState() => _HighscoreScreenState();
+}
+
+class _HighscoreScreenState extends State<HighscoreScreen> {
+  Map<NollningGroupRead, int> groupPointsMap = {};
+  NollningRead? nollning;
+
+  String avklarad = "assets/data/nollning_25/uppdrag/bricka-avklarad.png";
+  String bedomning = "assets/data/nollning_25/uppdrag/bricka-bedomning.png";
+  String misslyckad = "assets/data/nollning_25/uppdrag/bricka-misslyckad.png";
+  String start = "assets/data/nollning_25/uppdrag/bricka-start.png";
+  String rubrik = "assets/data/nollning_25/uppdrag/rubrik.png";
+
+  @override
+  void initState() {
+    super.initState();
+    _loadInitData();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void _loadInitData() async {
+    final nollningResponse = await ApiService.apiClient
+        .getNollningApi()
+        .nollningGetNollningByYear(year: DateTime.now().year);
+    final nollningData = nollningResponse.data;
+
+    if (nollningData != null) {
+      List<NollningGroupRead> mentorGroups = nollningData.nollningGroups
+          .where((group) => group.group.groupType.name == "mentor")
+          .toList();
+      for (final nollningGroup in mentorGroups) {
+        final groupMissions = await ApiService.apiClient
+            .getNollningApi()
+            .nollningGetGroupMissionsFromNollningGroup(
+              nollningGroupId: nollningGroup.id,
+            );
+
+        int totalPoints = 0;
+        if (groupMissions.data != null && groupMissions.data!.isNotEmpty) {
+          for (final gm in groupMissions.data!.toList()) {
+            if (gm.isAccepted == "Accepted") {
+              totalPoints += gm.points;
+            }
+          }
+        }
+        groupPointsMap[nollningGroup] = totalPoints;
+      }
+    }
+
+    setState(() {
+      this.nollning = nollningData;
+    });
+  }
+
+  List<Widget> _createHighscoreCards(BuildContext context) {
+    var t = AppLocalizations.of(context)!;
+    List<Widget> highscoreCards = [];
+
+    Map<NollningGroupRead, int> sortedMap = Map.fromEntries(
+      groupPointsMap.entries.toList()
+        ..sort((a, b) => b.value.compareTo(a.value)),
+    );
+
+    int pos = 1;
+
+    sortedMap.forEach((key, value) {
+      highscoreCards.add(highscoreCard(key, value, pos, context));
+      pos++;
+    });
+
+    return highscoreCards;
+  }
+
+  Widget highscoreCard(
+    NollningGroupRead group,
+    int points,
+    int position,
+    BuildContext context,
+  ) {
+    var t = AppLocalizations.of(context)!;
+    return Center(
+      child: Container(
+        height: widget.availableHeight / 15,
+        width: widget.availableWidth - 40,
+        child: Card(
+          shadowColor: Colors.transparent,
+          color: Colors.transparent,
+          child: Stack(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Center(
+                    child: Text(
+                      position.toString(),
+                      style: TextStyle(
+                        color: _getPositionColor(position),
+                        fontFamily: "VT323",
+                        fontWeight: FontWeight.w800,
+                        fontSize: widget.availableWidth / 15,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: widget.availableWidth / 3,
+                    height: widget.availableHeight / 10,
+                    child: Center(
+                      child: Text(
+                        textAlign: TextAlign.center,
+                        group.group.name.length <= 40
+                            ? group.group.name
+                            : group.group.name.substring(0, 20) + "...",
+                        style: TextStyle(
+                          fontFamily: "VT323",
+                          fontWeight: FontWeight.w600,
+                          fontSize: group.group.name.length <= 10
+                              ? widget.availableWidth / 15
+                              : (group.group.name.length <= 20
+                                    ? widget.availableWidth / 15
+                                    : widget.availableWidth / 30),
+                          color: _getPositionColor(position),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      _pointsToString(points),
+                      style: TextStyle(
+                        fontFamily: "VT323",
+                        fontWeight: FontWeight.normal,
+                        fontSize: widget.availableWidth / 15,
+                        color: _getPositionColor(position),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var t = AppLocalizations.of(context)!;
+
+    if (this.nollning == null) {
+      return Center(child: CircularProgressIndicator());
+    }
+
+    return Container(
+      height: widget.availableHeight,
+      width: widget.availableWidth,
+      child: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: widget.availableHeight * 0.01),
+                  child: Text(
+                    "HIGH SCORES",
+                    style: TextStyle(
+                      fontFamily: "VT323",
+                      fontSize: widget.availableWidth / 10,
+                      color: Colors.white, // justera efter din bakgrund
+                    ),
+                  ),
+                ),
+                ..._createHighscoreCards(context),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Color _getPositionColor(int pos) {
+    switch (pos % 4) {
+      case 0:
+        return Colors.indigo;
+      case 1:
+        return Colors.yellow;
+      case 2:
+        return Colors.pink;
+      default:
+        return Colors.cyan;
+    }
+  }
+
+  String _pointsToString(int points) {
+    String pointString = points.toString();
+    while (pointString.length < 6) {
+      pointString = "0" + pointString;
+    }
+    return pointString;
+  }
+}
--- lib/screens/nollning/questscreen_26/quest_home.dart
+++ lib/screens/nollning/questscreen_26/quest_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:fsek_mobile/l10n/app_localizations.dart';
+import 'package:fsek_mobile/screens/nollning/questscreen_26/highscore.dart';
+import 'package:fsek_mobile/screens/nollning/questscreen_26/quests.dart';
 
 class QuestHomeScreen extends StatefulWidget {
   @override
@@ -7,31 +8,111 @@ class QuestHomeScreen extends StatefulWidget {
 }
 
 class _QuestHomeScreenState extends State<QuestHomeScreen> {
-  void initState() {
-    super.initState();
+  String rubrik = "assets/data/nollning_26/uppdrag/rubrik1.png";
+  Color backgroundColor = Color(0xFF00487B);
+  TabController? _controller;
+
+  void _handleTabChange() {
+    setState(() {
+      rubrik = _controller!.index == 0
+          ? "assets/data/nollning_26/uppdrag/rubrik1.png"
+          : "assets/data/nollning_26/uppdrag/rubrik2.png";
+      backgroundColor = _controller!.index == 0
+          ? Color(0xFF00487B)
+          : Colors.black;
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller?.removeListener(_handleTabChange);
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    var t = AppLocalizations.of(context)!;
-
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        scrolledUnderElevation: 0.0,
-      ),
-      body: InteractiveViewer(
-        panEnabled: true,
-        child: Container(
-          height: screenHeight,
-          width: screenWidth,
-          child: Align(alignment: Alignment.center, child: Text("Sidequests!")),
-        ),
+    return DefaultTabController(
+      length: 2,
+      child: Builder(
+        builder: (context) {
+          final controller = DefaultTabController.of(context);
+          if (_controller != controller) {
+            _controller?.removeListener(_handleTabChange);
+            _controller = controller;
+            _controller?.addListener(_handleTabChange);
+          }
+          return Scaffold(
+            body: Container(
+              height: screenHeight,
+              width: screenWidth,
+              child: Stack(
+                children: [
+                  Positioned.fill(child: ColoredBox(color: backgroundColor)),
+                  Column(
+                    children: [
+                      Container(
+                        width: screenWidth,
+                        height: screenHeight / 8,
+                        child: Stack(
+                          children: [
+                            Positioned.fill(
+                              child: Padding(
+                                padding: EdgeInsets.only(
+                                  top: screenHeight * 0.03,
+                                  bottom: screenHeight * 0.01,
+                                ),
+                                child: Image.asset(rubrik, fit: BoxFit.fill),
+                              ),
+                            ),
+                            Positioned.fill(
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: GestureDetector(
+                                      behavior: HitTestBehavior.opaque,
+                                      onTap: () => _controller?.animateTo(0),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: GestureDetector(
+                                      behavior: HitTestBehavior.opaque,
+                                      onTap: () => _controller?.animateTo(1),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: screenWidth,
+                        height: screenHeight - (screenHeight / 8),
+                        child: TabBarView(
+                          children: [
+                            QuestScreen(
+                              availableHeight:
+                                  screenHeight - (screenHeight / 8),
+                              availableWidth: screenWidth,
+                            ),
+                            HighscoreScreen(
+                              availableHeight:
+                                  screenHeight - (screenHeight / 8),
+                              availableWidth: screenWidth,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
